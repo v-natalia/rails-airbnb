e2168dcddaa8dec65b477407b32b7e359e739d5c
nataliavega <nvegar@gmail.com>
2023-06-12 17:49:21 +0200

    index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'flats#index'
+  get 'flats/:id', to: 'flats#show', as: :flat
   # Defines the root path route ("/")
   # root "articles#index"
 end
---
+++ test/system/flats_test.rb
@@ -0,0 +1,17 @@
+require "application_system_test_case"
+
+class FlatsTest < ApplicationSystemTestCase
+  test "visiting the index" do
+    visit '/'
+
+    assert_selector "h1", text: "Flats"
+  end
+
+
+  test "visiting the show page" do
+    visit '/flats/145'
+
+    assert_selector "h2", text: "Charm at the Steps of the Sacre Coeur/Montmartre"
+  end
+
+end
